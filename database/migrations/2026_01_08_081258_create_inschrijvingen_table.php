7d46d1e7b506fda894d62ee40073b0d8a0b5f29e
Rowan Laffeber <9026051@student.zadkine.nl>
2026-01-13 15:17:38 +0100

    v0.1.6.1
    forgotten files

--- database/migrations/2026_01_08_081258_create_inschrijvingen_table.php
+++ database/migrations/2026_01_08_081258_create_inschrijvingen_table.php
@@ -16,25 +16,21 @@ return new class extends Migration
 
         $table->uuid('student_id');
 
-        $table->uuid('eerste_keuze_keuzedeel_id');
-        $table->uuid('tweede_keuze_keuzedeel_id')->nullable();
 
-        $table->uuid('toegewezen_keuzedeel_id')->nullable();
+        $table->uuid('keuzedeel_id')->nullable();
 
-        $table->boolean('afgerond')->default(false);
 
         $table->enum('status', [
             'ingediend',
             'goedgekeurd',
             'afgewezen',
+            'afgerond',
         ])->default('ingediend');
 
         $table->timestamps();
 
         $table->foreign('student_id')->references('id')->on('students')->cascadeOnDelete();
-        $table->foreign('eerste_keuze_keuzedeel_id')->references('id')->on('keuzedelen');
-        $table->foreign('tweede_keuze_keuzedeel_id')->references('id')->on('keuzedelen');
-        $table->foreign('toegewezen_keuzedeel_id')->references('id')->on('keuzedelen');
+        $table->foreign('keuzedeel_id')->references('id')->on('keuzedelen');
     });
 }
  
